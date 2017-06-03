002885da04bbf39d3eaad67093a54961da217e11
Ava Lee <avaklee@gmail.com>
2017-06-03 18:37:14 +0900

    Update on controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,17 @@
 class MessagesController < ApplicationController
-
+# before_action :move_to_index, except: :index
   def index
   end
-  
+
+
+
+private
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in?
+  # end
+
+  def message_params
+    params.permit(:name,:email)
+  end
+
 end

b30270135636fb5c425ac0ce5522ad2b05b44537
Ava Lee <avaklee@gmail.com>
2017-06-05 20:40:41 +0900

    Group機能の実装２
    #WHAT
    グループ作成機能を実装, 作成したグループをサイドバーに表示
    #WHY
    必要な機能なので

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,18 @@
 class GroupsController < ApplicationController
 
+  def index
+  end 
+
   def new
+    @group = Group.all
   end
 
   def edit
+  end
+
+private
+  def group_params
+    params.require(:group).permit(:name, user_ids:[])
   end
 
 end

83fc9d514b9081699b343fe9fe93aac78a9ca9d0
Ava Lee <avaklee@gmail.com>
2017-06-07 14:50:38 +0900

    message送信機能修正
    # WHAT
    バリデーションの設定とレンダーの追加
    # WHY
    Image or bodyどちらかがあればokになるようにするため、レンダーは前回のミス

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,9 +3,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to controller: 'groups', action: 'index'
+      redirect_to root_path
     else
       flash[:alert] = "メッセージを入力してください。"
+      render　:create
     end
   end
 

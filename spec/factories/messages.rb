f97798a3b62b50a1bac3ef4846feba3f967216e5
Ava Lee <avaklee@gmail.com>
2017-06-12 11:27:51 +0900

    Use of Faker
    # What
    Use Faker for Body
    
    #Why
    To test

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :message do
-    body "Hi, world"
+    body {Faker::Lorem.sentence}
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'files', 'test.jpg'))}
     group_id 1
     user_id 2

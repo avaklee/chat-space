368ad5712f19f93973d405697c3c777092f5072e
Ava Lee <avaklee@gmail.com>
2017-06-14 17:03:09 +0900

    メッセージ送信機能実装の非同期通信
    # WHAT
    
    メッセージ送信機能実装の非同期通信
    カリキュラムの１−９の作業
    
    #WHY
    
    非同期通信ではビューは再描画せず、必要なデータを返し、返したデータJavaScriptで扱ってビューの一部を変更するので、同じ時間で処理できる仕
    事量が増え性能が向上するため

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,44 @@
+$(function() {
+  function buildHTML(message) {
+    var html =
+    '<div class="right_content__middle_content__message">'
+      + '<div class="right_content__middle_content__message__user_name">'
+      + message.user_name
+      +'</div>'
+      + '<div class="right_content__middle_content__message__date">'
+      + message.created_at
+      +'</div>'
+      + '<div class="right_content__middle_content__message__comment">'
+      + message.body
+      + '</div>'
+    + '</div>'
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+     var formData = new FormData($(this).get(0));
+     var url = $(this).get(0).action;
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      var position = $('#new_message').offset().top;
+      $('.right_content__middle_content').append(html);
+      $('#message_body').val('');
+      $("html, body").animate({scrollTop : position}, {
+        queue : false
+      });
+    });
+
+    .fail(function(data) {
+      alert(data)
+    });
+  });
+});
